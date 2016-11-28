ff50586c6580ebedbf9ca524d24b1800e9815157
Daniel <danielmolenaar@aol.nl>
2016-11-28 21:53:29 +0100

    Fixed imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from dogecoin import dogecoin
-from bitcoin import bitcoin
-from auth import auth
 
 app = Flask(__name__)
+
+from app.dogecoin import dogecoin
+from app.bitcoin import bitcoin
+from app.auth import auth
+
 app.register_blueprint(bitcoin)
 app.register_blueprint(dogecoin)
 app.register_blueprint(auth)
