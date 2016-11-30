12a465b2f84797dcbd3856a89a15c5e449c6e2ae
bart-rijnders <mail@bartrijnders.com>
2016-11-30 16:08:00 +0100

    added demonize

--- run.py
+++ run.py
@@ -2,9 +2,16 @@ from time import sleep
 from daemonize import Daemonize
 from app import app
 from config import *
+import sys
+import logging
+
 pid = "/tmp/server.pid"
 
 def main():
+    app.debug == True
+    handler = logging.FileHandler("/tmp/coinapplication.log", "w+")
+    log = logging.getLogger('werkzeug')
+    log.addHandler(handler)
     app.run(HOSTNAME, PORT)
 
 daemon = Daemonize(app="coinapplication_server", pid=pid, action=main)
