2b4a40b3c8109e59f29c2233ff57d1d727614697
bart-rijnders <mail@bartrijnders.com>
2016-11-30 23:41:59 +0100

    fixed __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,6 @@ app.register_blueprint(bitcoin)
 app.register_blueprint(dogecoin)
 app.register_blueprint(auth)
 app.debug = True
-sys.stdout = sys.stderr
 
 if __name__ == "__main__":
     app.run(debug=True)
