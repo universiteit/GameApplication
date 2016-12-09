e803c53a4b50c9218fced58f6f03a2edf1dcaf9d
Daniel <danielmolenaar@aol.nl>
2016-12-10 00:31:44 +0100

    Added exception handling to jwt parser

--- app/auth/attributes.py
+++ app/auth/attributes.py
@@ -26,21 +26,35 @@ _context = None
 def context():
     return _context
 
+def _parse_token(auth):
+    if auth is None:
+        return 'No authorization header was found. The request has to be authenticated to access secured resources.', 401
+    split = auth.split()
+    if len(split) != 2:
+        return 'Authorization header was not formatted correctly.', 400
+    if split[0].lower() != 'bearer':
+        return 'Only Bearer tokens are supported in the authentication header.', 400
+
+def _get_token(auth):
+    return auth.split()[1]
 
 def secure(handle_unauthorized=True):
     def secure_decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            authorization = request.headers.get('Authorization')
+            authorization = request.headers.get('Authorization', None)
+            err = _parse_token(authorization)
+            if err != None: return err
 
-            if authorization is None:
-                return 'No authorization header set. The request has to be authenticated to access secured resources.', 401
-            
+            token_string = _get_token(authorization)
+
             token = None
             try:
-                token = jwt.decode(authorization, SECRET, verify=VERIFY, algorithms=ALGORITHM, options=CHECKS, leeway=LEEWAY)
+                token = jwt.decode(token_string, SECRET, verify=VERIFY, algorithms=ALGORITHM, options=CHECKS, leeway=LEEWAY)
             except ExpiredSignatureError:
                 return 'The authentication token has expired.', 401
+            except:
+                return 'Something went wrong when parsing the authentication header.', 400
 
             global _context
             _context = TokenContext(token)
