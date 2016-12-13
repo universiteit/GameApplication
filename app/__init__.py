61e2df8bd728fc18bf9555000eff40ebe47b0c79
bart-rijnders <mail@bartrijnders.com>
2016-12-13 16:10:52 +0100

    Added authentication with sessions
    Added session authentication in addition to JWT

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,7 @@ with app.app_context():
 # Register blueprints
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(rts, url_prefix='/rts')
+app.secret_key = "secret test key"
 
 if __name__ == "__main__":
     app.run(debug=True)
