7d1b67eeed42a753b1835ba29e0999108994e276
bart-rijnders <Moro@Barts-iMac.local>
2016-12-28 21:14:53 +0100

    Added preliminary test for db persistence

--- app/RTS/tests/test_db.py
+++ app/RTS/tests/test_db.py
@@ -1,27 +1,30 @@
 import unittest
 from app import app, db
 from app import bcrypt
-from app.RTS.models.player import Player
+from app.RTS.models import *
 from app.auth.models.user import User
 
-@unittest.skip("Not ready yet")
+
 class TestDb(unittest.TestCase):
     def setUp(self):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
         with app.app_context():
+            db.drop_all()
             db.create_all()
 
         # Create mock user
         password = bcrypt.generate_password_hash("password")
-        self.user = User("test_user", password)
-        db.session.insert(user)
+        self.user = User("test_user_1", password)
+        self.user2 = User("test_user_2", password)
+        db.session.add(self.user)
+        db.session.add(self.user2)
         db.session.commit()
 
     def test_player(self):
         player = Player(self.user, "Lannister")
         db.session.add(player)
         db.session.commit()
-        result = Player.query.filter_by(username="Jaime").first()
+        result = Player.query.first()
         self.assertIsNotNone(result)
         db.session.delete(player)
         db.session.commit()
@@ -38,8 +41,32 @@ class TestDb(unittest.TestCase):
 
         db.session.delete(town)
         db.session.delete(player)
-        db.commit()
+        db.session.commit()
+
+    def test_player_town_relationship(self):
+        player1 = Player(self.user, "Lannister")
+        player2 = Player(self.user2, "Stark")
+        db.session.add(player1)
+        db.session.add(player2)
+        db.session.commit()
         
+        town = Town(player1, "Casterly Rock")
+        db.session.add(town)
+        db.session.commit()
+
+        town.player = player2
+        db.session.add(town)
+        db.session.commit()
+
+        result = Town.query.filter_by(name="Casterly Rock").first()
+        self.assertEqual(result.player.user.username, player2.user.username)
+
+        player1.towns.append(town)
+        db.session.add(player1)
+        db.session.commit()
+
+        result = Town.query.filter_by(name="Casterly Rock").first()
+        self.assertEqual(result.player.user.username, player1.user.username)
+
     def tearDown(self):
         db.session.delete(self.user)
-        db.session.commit()
