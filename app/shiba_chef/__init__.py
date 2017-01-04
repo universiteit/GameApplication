5c00e3da8039d32dffa29ad5623b300ba746931c
Daniel <danielmolenaar@aol.nl>
2017-01-04 23:49:59 +0100

    Added highscore model

--- app/shiba_chef/__init__.py
+++ app/shiba_chef/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request
 from app import db
 
+from app.shiba_chef.models.highscore import Highscore
+
 shiba_chef = Blueprint('Shiba Chef game', __name__, static_folder='static', template_folder='templates')
 
 
@@ -11,7 +13,18 @@ def index():
 @shiba_chef.route('/score', methods=['POST'])
 def send_highscore():
     content = request.get_json()
-    print(content)
+    user_id = request.session.id
+
+    highscore = Highscore.filter_by(id=user_id).first()
+    if highscore is None:
+        highscore = Highscore(user_id, content.score)
+        db.session.add(highscore)
+        db.session.commit()
+    elif content.score > highscore:
+        highscore.highscore = content.score
+        db.session.commit()
+
+
     return str(content), 200
 
 # Static file routing
