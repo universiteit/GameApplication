9f449ea083ea8d748153f894627c81fbf12e6aed
Jeffrey Maas <jmaasy@gmail.com>
2017-01-05 17:07:00 +0100

    Added additional unit tests
    Added additional unit tests for the posts and the views

--- app/RTS/tests/test_views.py
+++ app/RTS/tests/test_views.py
@@ -19,6 +19,7 @@ class TestViews(unittest.TestCase):
         
         password = bcrypt.generate_password_hash("password")
         self.user = User("test", password)
+        self.player = Player(self.user, "Lannister")
         
         db.session.add(self.user)
         db.session.commit()
@@ -60,4 +61,54 @@ class TestViews(unittest.TestCase):
         rv = self.app.get('/rts/towns', follow_redirects = True)
         assert b'Lannisport' in rv.data
 
-    
+    @mock.patch('app.RTS.views.current_player')
+    def test_house(self, mock_current_player):
+        self.create_session()
+
+        mock_current_player.return_value = None
+        rv = self.app.get('/rts/house', follow_redirects = True)
+        assert b'You have not yet created' in rv.data
+
+        mock_current_player.return_value = Player(self.user, "Lannister")
+        rv = self.app.get('/rts/house', follow_redirects = True)
+        assert b'Lannister' in rv.data
+
+    @mock.patch('app.RTS.views.Town')
+    @mock.patch('app.RTS.views.current_player')
+    def test_town_view(self, mock_current_player, mock_current_town):
+        self.create_session()
+
+        mock_player = Player(self.user, "Lannister")
+        mock_current_player.return_value = None
+
+        town = Town(mock_player, "Lannisport")
+        mock_first = mock.MagicMock()
+        mock_first.first = mock.MagicMock(return_value = town)
+        mock_current_town.query.filter_by = mock.MagicMock(return_value = mock_first)
+        self.assertNotEqual(mock_current_player, mock_current_town.player)
+        rv = self.app.get('/rts/town/1', follow_redirects = True)
+        assert b'Owned by' in rv.data
+
+        mock_current_player.return_value = mock_player
+        self.assertEqual(mock_player, town.player)
+        rv = self.app.get('/rts/town/1', follow_redirects = True)
+        assert b'Lannisport' in rv.data
+
+    @mock.patch('app.RTS.views.current_player')
+    @mock.patch('app.RTS.views.Attack')
+    def test_attackview(self, mock_current_attack, mock_current_player):
+        self.create_session()
+
+        mock_current_player.return_value = None
+        rv = self.app.get('/rts/attacks', follow_redirects = True)
+        assert b'You have not yet created' in rv.data
+
+        player = Player(self.user, "Lannister")
+        town = Town(player, "Lannisport")
+
+        mock_current_player.return_value = player
+        mock_first = mock.MagicMock()
+        mock_first.all = mock.MagicMock(return_value=[Attack(player, town, town)])
+        mock_current_attack.query.filter = mock.MagicMock(return_value=mock_first)
+        rv = self.app.get('/rts/attacks', follow_redirects=True)
+        assert b'Lannisport' in rv.data
--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-# Flask
+ # Flask
 from flask import Flask
 app = Flask(__name__, static_folder='static')
 app.config.from_object('config')
